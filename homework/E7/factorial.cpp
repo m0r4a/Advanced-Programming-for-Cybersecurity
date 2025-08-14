73df2c6d2196b50f5e7a5ed403ec8557c6d0e2c0
m0r4a <m0r4a@proton.me>
2025-08-13 23:45:16 -0600

    E7 finished

---
+++ homework/E7/factorial.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+// long: signed integer data type with extended size (minimum 32 bits, typically 32 or 64 bits depending on platform)
+// Use for: large integer values that exceed int range, file sizes, populations, distances
+
+long factorial(int x) {
+  if (x<2) {
+    return 1;
+  }
+
+  return x * factorial(x-1);
+}
+
+int main() {
+  int num;
+  
+  cout << " Calcular factorial de: ";
+  cin >> num;
+
+  cout << "\n " << num << " != " << factorial(num) << endl;
+
+  return 0;
+}
