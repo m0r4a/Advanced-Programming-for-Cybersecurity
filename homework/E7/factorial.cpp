8a02c4db216056e725f578ca931fa0aaa97b03ca
m0r4a <m0r4a@proton.me>
2025-08-13 23:46:31 -0600

    Add litte transparency

--- homework/E7/factorial.cpp
+++ homework/E7/factorial.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 // long: signed integer data type with extended size (minimum 32 bits, typically 32 or 64 bits depending on platform)
 // Use for: large integer values that exceed int range, file sizes, populations, distances
+// comment AI generated btw
 
 long factorial(int x) {
   if (x<2) {
